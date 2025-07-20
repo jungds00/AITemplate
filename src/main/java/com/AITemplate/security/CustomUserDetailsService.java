9e5038c024b805b883a4a25471ced69becc27600
jungds00 <84696143+jungds00@users.noreply.github.com>
2025-07-20 15:17:40 +0900

    Update

--- src/main/java/com/AITemplate/security/CustomUserDetailsService.java
+++ src/main/java/com/AITemplate/security/CustomUserDetailsService.java
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     public UserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
         User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with userId: " + userId));
+                .orElseThrow(() -> new UsernameNotFoundException("userId로 사용자를 찾을 수 없습니다 : " + userId));
 
         return UserPrincipal.create(user);
     }
